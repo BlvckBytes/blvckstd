f29b0924c10c6b9e1bed981d5d7ea20667c04bfd
Philip Hell <blvckbytes@gmail.com>
2022-03-08 05:52:21 +0100

    atomanip: fixed esp8266 support

--- src/atomanip.cpp
+++ src/atomanip.cpp
@@ -4,17 +4,19 @@ size_t atomic_add(volatile size_t *target, const size_t value)
 {
   size_t old, n;
 
+  #ifdef ESP8266
+  n = value;
+  *target = n;
+  #else
+
   // Try to compare and swap atomically until succeeded
   do {
     old = *target;
     n = old + value;
   } while (
-    #ifdef ESP8266
-    false
-    #else
     !__sync_bool_compare_and_swap(target, old, n)
-    #endif
   );
+  #endif
 
   // Return the new value
   return n;
